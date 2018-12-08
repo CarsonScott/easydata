0a6112a337844241634a6263162025427a2d62be
Carson Scott <carsonjscott14@gmail.com>
2018-12-08 12:38:30 -0500

    updated package description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='easydata',
     version='1.0',
-    description='A simple and lightweight database and graph library.',
+    description='A simple and lightweight database/graph library in python.',
     url='https://github.com/CarsonScott/easydata',
     author='Carson Scott',
     author_email='carsonjscott14@gmail.com',
